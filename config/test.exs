90cec6c972ef92933bf9304f090847e28362a47d
Tony Collen <tony@halogenlabs.com>
2019-01-17 21:40:12 -0600

    Remove test db credentials

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,6 @@ config :logger, level: :warn
 # Configure your database
 config :aprsme, Aprsme.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "aprsme_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
